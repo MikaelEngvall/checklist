3141d8b92e76ca58279925128694afa70dd0dd3e
Mikael Engvall <mikael.engvall.me@gmail.com>
2024-12-31 20:36:39 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+// React v18
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
